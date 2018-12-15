8c1fee08c57d63b4ddfde2a2c6eb5242055ba7f7
krishna281 <42407923+krishna281@users.noreply.github.com>
2018-12-15 14:57:42 +0530

    project add

---
+++ resources/views/home.blade.php
@@ -0,0 +1,224 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>The Attorney Group System</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+
+
+
+
+<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>  
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+
+	</head>
+	<body>
+		
+	<div class="colorlib-loader"></div>
+	
+	<div id="page">
+	<nav class="colorlib-nav" role="navigation">
+		<div class="top-menu">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-2">
+						<div id="colorlib-logo"><a href="index.html">AttorneySystem</a></div>
+					</div>
+					<div class="col-md-10 text-right menu-1">
+						<ul>
+							<li class="active"><a href="index.html">Home</a></li>
+							<li><a href="#">Won Cases</a></li>
+							<li><a href="#">Lawyer</a></li>
+				<!--		<li class="has-dropdown">
+								<a href="#">Blog</a>
+								       
+							</li>		--> 
+							<li><a href="#">About</a></li>
+							<li><a href="#">Contact</a></li>
+							<li class="btn-cta"><a href="#"><span>Make an Appointment</span></a></li>
+							<li class="btn-cta"><a href="#"><span>Login</span></a></li>
+							<!-- <li class="btn-cta"><a href="#"><span>Sign Up</span></a></li> -->
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</nav>
+
+	<aside id="colorlib-hero" class="js-fullheight">
+		<div class="flexslider js-fullheight">
+			<ul class="slides">
+		   	<li style="background-image: url(images/img_bg_1.jpg);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="row">
+			   			<div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+			   				<div class="slider-text-inner">
+			   					<h1>We help to solve business law solution</h1>
+									<h2>Your Legal Solution starts here!</h2>
+									<p><a class="btn btn-primary btn-lg" href="#">Make An Appointment</a></p>
+			   				</div>
+			   			</div>
+			   		</div>
+		   		</div>
+		   	</li>
+		   	<li style="background-image: url(images/img_bg_2.jpg);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="row">
+			   			<div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+			   				<div class="slider-text-inner">
+			   					<h1>Your ultimate law &amp; legal Sulution</h1>
+									<h2>Visit our sites here! <a href="#" target="_blank">The Attorney Group System.com</a></h2>
+									<p><a class="btn btn-primary btn-lg btn-learn" href="#">Make An Appointment</a></p>
+			   				</div>
+			   			</div>
+			   		</div>
+		   		</div>
+		   	</li>
+		   	<li style="background-image: url(images/img_bg_3.jpg);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="row">
+			   			<div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+			   				<div class="slider-text-inner">
+			   					<h1>Defend Your Constitutional Right with Legal Help</h1>
+									<h2>Visit our sites here! <a href="#" target="_blank">The Attorney Group System.com</a></h2>
+									<p><a class="btn btn-primary btn-lg btn-learn" href="#">Make An Appointment</a></p>
+			   				</div>
+			   			</div>
+			   		</div>
+		   		</div>
+		   	</li>		   	
+		  	</ul>
+	  	</div>
+	</aside>
+	<div id="intro-bg">
+		<div class="container">
+			<div id="colorlib-intro">
+				<div class="third-col">
+					<span class="icon"><i class="icon-cog"></i></span>
+					<h2>Need Legal Services?</h2>
+					<p>It is an Act to constitute legal services authorities to provide free and competent legal services to the weaker sections of the society to ensure that opportunities for securing justice are not denied to any citizen by reason of economic or other disabilities, and to organize Lok Adalats to secure that the operation.</p>
+				</div>
+				<div class="third-col third-col-color">
+					<span class="icon"><i class="icon-old-phone"></i></span>
+					<h2>Call now (+91) 9825181374</h2>
+					<h2>Email us @ <a href="#">s.kamlesh69@ymail.com</a></h2>
+		<!--			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, iure delectus. Ab nihil est ut quae, atque,</p>   -->
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+
+	<footer id="colorlib-footer" role="contentinfo">
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-3 colorlib-widget">
+					<h4>The Attorney Group System</h4>
+					<p>A lawyer or attorney is a person who practices law, as an advocate, attorney, ... Some jurisdictions have two types of lawyers, barristers and solicitors. Whilst others fuse the two.</p>
+				</div>
+				<div class="col-md-3 col-md-push-1">
+					<h4>Navigation</h4>
+					<ul class="colorlib-footer-links">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Won Cases</a></li>
+						<li><a href="#">Lawyer</a></li>
+		<!--			<li><a href="#">Blog</a></li>		-->
+						<li><a href="#">About us</a></li>
+					</ul>
+				</div>
+
+				<div class="col-md-3 col-md-push-1">
+					<h4>Contact Information</h4>
+					<ul class="colorlib-footer-links">
+						<li>U/3,Ratneep Complex Opp.Gloria Restaurant, <br> Opp.100 FT Ring Road,Satellite</li>
+						<li><a href="tel://1234567920">(+91) 9825181374</a></li>
+						<li><a href="mailto:info@yoursite.com">s.kamlesh69@ymail.com</a></li>
+					</ul>
+				</div>
+
+				<div class="col-md-3 col-md-push-1">
+					<h4>Opening Hours</h4>
+					<ul class="colorlib-footer-links">
+						<li>Mon - Fri: 10:00 - 20:00</li>
+						<li>Sat 9:30 - 18:00</li>
+						<li>Sun 10:00 - 13:00</li>
+					</ul>
+				</div>
+
+			</div>
+
+			<div class="row">
+				<div class="col-md-12 text-center">
+					
+					<p>
+						<ul class="colorlib-social-icons">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+
+	
+	</body>
+</html>
+
